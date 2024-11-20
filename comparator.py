bde77cb637841cc473eca291f6639ef83b11c37a
megahomyak <g.megahomyak@gmail.com>
2024-11-21 01:44:12 +0500

    Better output

--- comparator.py
+++ comparator.py
@@ -2,23 +2,25 @@ import subprocess
 import re
 
 def execute(command_parts):
-    return subprocess.run(command_parts, capture_output=True).stdout.decode()
+    return subprocess.run(command_parts, capture_output=True).stdout.decode().strip()
 
 class Exit(Exception):
     pass
 
-def prompt_for_luminosity():
-    match = re.match(r"#(..)(..)(..)", execute(["gpick", "-s", "-o"]))
+def prompt_for_luminosity(output_prefix):
+    color_hex = execute(["gpick", "-s", "-o"])
+    match = re.match(r"#(..)(..)(..)", color_hex)
     if match is None:
         raise Exit()
+    print(f"{output_prefix}: {color_hex}")
     r, g, b = map(lambda n: int(n, 16), match.groups())
     luminosity = 0.299*r + 0.587*g + 0.114*b
     return luminosity
 
 try:
     while True:
-        old_luminosity = prompt_for_luminosity()
-        new_luminosity = prompt_for_luminosity()
-        print(f"old - new = {old_luminosity - new_luminosity}")
+        old_luminosity = prompt_for_luminosity("1. Old")
+        new_luminosity = prompt_for_luminosity("2. New")
+        print(f"3. old - new = {old_luminosity - new_luminosity}")
 except Exit:
     pass
