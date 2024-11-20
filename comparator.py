95561e74fec88a7d063035b58001758752caa4e4
megahomyak <g.megahomyak@gmail.com>
2024-11-21 02:02:46 +0500

    Implemented another mode for the program

--- comparator.py
+++ comparator.py
@@ -1,5 +1,11 @@
 import subprocess
 import re
+import sys
+
+try:
+    mode = sys.argv[1]
+except IndexError:
+    mode = "old_new"
 
 def execute(command_parts):
     return subprocess.run(command_parts, capture_output=True).stdout.decode().strip()
@@ -18,9 +24,15 @@ def prompt_for_luminosity(output_prefix):
     return luminosity
 
 try:
-    while True:
+    if mode == "old_new":
+        while True:
+            old_luminosity = prompt_for_luminosity("1. Old")
+            new_luminosity = prompt_for_luminosity("2. New")
+            print(f"3. old - new = {old_luminosity - new_luminosity}")
+    else if mode == "only_new":
         old_luminosity = prompt_for_luminosity("1. Old")
-        new_luminosity = prompt_for_luminosity("2. New")
-        print(f"3. old - new = {old_luminosity - new_luminosity}")
+        while True:
+            new_luminosity = prompt_for_luminosity("2. New")
+            print(f"3. old - new = {old_luminosity - new_luminosity}")
 except Exit:
     pass
